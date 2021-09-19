63f81f0e1370a188a0057670f3456ca313a02cad
Billybishop <30381194+Billybishop@users.noreply.github.com>
2021-09-19 05:38:08 -0400

    Updated Aimbot features
    Added rotation rate property that's used for smoothing the aiming speed, changed key for Aimbot activation to right mouse, added property for limiting Aimbot field of view, added complete bone enum for future use in bone scanning.

--- Features/Aimbot.cs
+++ Features/Aimbot.cs
@@ -8,10 +8,49 @@ namespace EFT.Trainer.Features
 {
 	public class Aimbot : HoldMonoBehaviour
 	{
-		public override KeyCode Key { get; set; } = KeyCode.Slash;
+		public override KeyCode Key { get; set; } = KeyCode.Mouse1; //Only execute Aimbot while right mouse button is held
 
 		[ConfigurationProperty]
-		public float MaximumDistance { get; set; } = 200f;
+		public float MaximumDistance { get; set; } = 300f;
+		public float RotationRate { get; set; } = 8f; //Value to smooth the rotation of Aimbot
+		public float MaximumAimField { get; set; } = 16f; //Value to limit Aimbot's Field of View (0-360, angle)
+		
+		public enum TargetBones {
+			Head,
+			Neck,
+			Spine3,
+			LeftShoulder,
+			RightShoulder,
+			Ribcage,
+			Spine2,
+			RightUpperarm,
+			LeftUpperarm,
+			Stomach,
+			Spine1,
+			RightForearm,
+			LeftForearm,
+			Spine1,
+			Pelvis,
+			RightPalm,
+			LeftPalm,
+			RightThigh2,
+			LeftThigh2,
+			RightCalf,
+			LeftCalf,
+			RightFoot,
+			LeftFoot
+		}
+		
+		/**
+		 * (BillyBishop) TODO:
+		 *
+		 *	   * Implement the property 'MaximumAimField' to limit the Aimbot's Field of View.
+		 *
+		 *     * Add and implement 'GetBestBone(player)' for dynamically targeting bones.
+		 *	     - It should return the first visible bone position of a given player, iterating in this order:  head>body>limbs
+		 *		 - Make use of the 'TargetBones' for full list of potential bones
+		 *		 - Returns empty vec3 if no bones were visible
+		 **/
 
 		protected override void UpdateWhenHold()
 		{
@@ -70,13 +109,13 @@ namespace EFT.Trainer.Features
 		{
 			var delta = player.Fireport.position - player.Fireport.up * 1f;
 			var eulerAngles = Quaternion.LookRotation((position - delta).normalized).eulerAngles;
-
+			
 			if (eulerAngles.x > 180f)
 				eulerAngles.x -= 360f;
 
-			player.MovementContext.Rotation = new Vector2(eulerAngles.y, eulerAngles.x);
+			player.MovementContext.Rotation = new Vector2(eulerAngles.y / RotationRate, eulerAngles.x / RotationRate);
 		}
-
+	
 		public static Vector3 GetHeadPosition(Player player)
 		{
 			var bones = player.PlayerBones;
